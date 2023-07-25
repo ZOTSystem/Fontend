279993e49a1e9b7bc1fa6018cb73fb7848cbe845
sam <minh1809.eco@gmail.com>
2023-07-25 17:02:55 +0700

    implement ui for edit comment

--- src/Component/User/CommentList.jsx
+++ src/Component/User/CommentList.jsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
 import Comment from './ForumComponent/Comment';
 
 const CommentList = ({ comments }) => {
+    const [editingCommentId, setEditingCommentId] = useState(null);
+
+    const handleEditComment = (comment) => {
+        setEditingCommentId(comment.postCommentId);
+    };
+
+    const handleCancelEditMode = () => {
+        setEditingCommentId(null);
+    };
+
+    const handleSaveComment = (comment, updatedContent) => {
+        // Handle saving the updated content here (you can use the comment.postCommentId and updatedContent)
+        // For example: Call an API to update the comment on the server
+        console.log(comment.postCommentId, updatedContent);
+
+        setEditingCommentId(null);
+    };
+
     return (
         <div className="comment-list">
             {comments?.map((comment) => (
-                <Comment key={comment.postCommentId} comment={comment} />
+                <Comment
+                    key={comment.postCommentId}
+                    comment={comment}
+                    isEditing={editingCommentId === comment.postCommentId}
+                    onEditComment={handleEditComment}
+                    onCancelEditMode={handleCancelEditMode}
+                    onSaveComment={handleSaveComment}
+                />
             ))}
         </div>
     );
--- src/Component/User/ForumComponent/Comment.jsx
+++ src/Component/User/ForumComponent/Comment.jsx
@@ -1,11 +1,26 @@
+import { useEffect, useRef, useState } from 'react';
 import { Avatar } from 'antd';
 import 'bootstrap/dist/css/bootstrap.css';
-import React, { useState } from 'react';
+import { getPointerContentEditable, setEndOfContentEditable } from '../../../utils/focusEditable';
 const url = '../Image/Forum/forum-avatar1.png';
 const baitoan = '../Image/Forum/baitoan.png';
 
-export default function Comment({ comment }) {
+export default function Comment({ comment, isEditing, onEditComment, onCancelEditMode, onSaveComment }) {
     const { fullName, content } = comment;
+    const contentRef = useRef();
+    const caretPos = useRef();
+    const [textContent, setTextContent] = useState(content);
+
+    const handleEditComment = () => {
+        onSaveComment(comment, contentRef.current.textContent);
+        contentRef.current.focus();
+    };
+
+    useEffect(() => {
+        if (isEditing) {
+            setEndOfContentEditable(contentRef.current);
+        }
+    }, [textContent, isEditing]);
 
     return (
         <div className="comment-wrapper">
@@ -15,15 +30,35 @@ export default function Comment({ comment }) {
             <div className="comment-right">
                 <div className="comment-content">
                     <div>{fullName} • 15 giờ trước</div>
-                    <div>
-                        <p>{content}</p>
+
+                    <p
+                        ref={contentRef}
+                        contentEditable={isEditing}
+                        suppressContentEditableWarning={true}
+                        onInput={(e) => {
+                            caretPos.current = getPointerContentEditable(contentRef.current);
+                            setTextContent(e.target.textContent);
+                        }}>
+                        {content}
+                    </p>
+                </div>
+                {!isEditing ? (
+                    <div className="comment-action">
+                        <button className="comment-action-btn" onClick={() => onEditComment(comment)}>
+                            Chỉnh sửa
+                        </button>
+                        <button className="comment-action-btn">Xóa</button>
                     </div>
-                </div>
-                {/* <img src={baitoan}></img> */}
-                <div className="comment-action">
-                    <button className="comment-action-btn">Chỉnh sửa</button>
-                    <button className="comment-action-btn">Xóa</button>
-                </div>
+                ) : (
+                    <div className="comment-action">
+                        <button className="comment-action-btn" onClick={handleEditComment}>
+                            Lưu
+                        </button>
+                        <button className="comment-action-btn" onClick={onCancelEditMode}>
+                            Hủy
+                        </button>
+                    </div>
+                )}
             </div>
         </div>
     );
